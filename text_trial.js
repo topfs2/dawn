af9986ad400489f5145df8ead98405c13ee46eec
Tobias Arrskog <topfs2@kodi.tv>
2015-07-04 11:45:16 +0200

    Sample markup

--- text_trial.js
+++ text_trial.js
@@ -1,6 +1,7 @@
 var camera = new dawn.OrthographicCamera(4 * 16 / 9, 4, -1, 1000);
 
-var layout = new dawn.TextLayout("Hello world!", "Droid sans light 72", new dawn.vec4(1, 1, 1, 1), 1);
+var s = '<span color="orange" font="24">YEAR</span>1983\n147<span color="orange" font="24">MIN</span>\nMP3\n36<span color="orange" font="24">TRACKS</span>'
+var layout = new dawn.TextLayout(s, "Ubuntu bold 72", new dawn.vec4(1, 1, 1, 1), 1);
 var text_image = new dawn.TextImage(layout);
 
 var mesh = new dawn.Mesh3D(new dawn.PlaneGeometry(text_image.width, text_image.height), new dawn.ShaderMaterial("shaders/map"));
@@ -8,23 +9,11 @@ mesh.material.uniform("map", text_image);
 
 var scene = new dawn.Scene3D(camera, mesh, 1280, 720);
 
-var time = function () {
-    return (new Date()).getTime();
-};
-
 resize = function (width, height) {
     scene.camera.projection(width, height, -1.0, 1000.0);
     scene.size(width, height);
 }
 
-var frame = 0;
-var start = time();
 update = function () {
-    var now = time();
-    var t = Math.floor((now - start) / 1000);
-
-    layout.text = "Hello World " + t;
-    mesh.geometry.size(text_image.width, text_image.height);
-
     return scene.instance;
 }
